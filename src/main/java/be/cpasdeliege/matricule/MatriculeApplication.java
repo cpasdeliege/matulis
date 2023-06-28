a527c1b7bc94d4d188669f36a52b502846a89534
Pythorogus <pierre.gomba@gmail.com>
2023-06-28 09:14:03 +0200

    1er commit

---
+++ src/main/java/be/cpasdeliege/matricule/MatriculeApplication.java
@@ -0,0 +1,13 @@
+package be.cpasdeliege.matricule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/cpasdeliege/matricule/MatriculeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.cpasdeliege.matricule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
