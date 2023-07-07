0cb244ae8460a7a95c59705052ddb1f4a8156bcd
Pythorogus <pierre.gomba@gmail.com>
2023-07-07 15:05:43 +0200

    suite jwt auth

---
+++ src/main/java/be/cpasdeliege/auth/model/User.java
@@ -0,0 +1,65 @@
+package be.cpasdeliege.auth.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+	
+	@Id
+	private Integer id;
+	
+	private String firstname;
+	
+	private String lastname;
+	
+	private String email;
+	
+	private String password;
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
