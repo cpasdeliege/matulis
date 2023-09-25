5dd4da3688671187e7c6471fc035a5879b646273
Pythorogus <pierre.gomba@gmail.com>
2023-09-25 13:59:07 +0200

    redirection to home if authenticated

--- frontend/src/app/users/components/login.component.ts
+++ frontend/src/app/users/components/login.component.ts
@@ -27,7 +27,9 @@ export class LoginComponent implements OnInit, OnDestroy {
 	) {}
   
 	ngOnInit() {
-
+		if(this.authenticationService.isAuthenticated()){
+			this.navigationService.redirect('/home');
+		}
 	}
 
 	ngOnDestroy() {
