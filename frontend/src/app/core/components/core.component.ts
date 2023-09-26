808b83c930bbb51d106d765089f0cb255ff15b08
Pythorogus <pierre.gomba@gmail.com>
2023-09-26 11:12:36 +0200

    title

--- frontend/src/app/core/components/core.component.ts
+++ frontend/src/app/core/components/core.component.ts
@@ -4,6 +4,7 @@ import { Authentication } from 'src/app/users/model/Authentication';
 import { AuthenticationService } from 'src/app/users/services/authentication.service';
 import { NavigationService } from 'src/app/shared/services/navigation.service';
 import { ToolService } from 'src/app/shared/services/tool.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'core-root',
@@ -18,12 +19,16 @@ export class CoreComponent implements OnInit, OnDestroy {
 	constructor(
 		private authenticationService:AuthenticationService,
 		private toolService:ToolService,
-		private navigationService:NavigationService
+		private navigationService:NavigationService,
+		private title:Title
 	){}
 
 	ngOnInit() {
 		// INIT APP START
 		this.navigationService.init();
+
+		// Meta title du site
+		this.title.setTitle("MATULIS - CPAS de Liège");
 
 		// AUTHENTICATION
 
